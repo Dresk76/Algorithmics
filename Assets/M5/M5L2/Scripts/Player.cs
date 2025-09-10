11f891608b1c24e45f1c14a0909a437df30bbd01
Felipe Agudelo <felipeagudeloolaya@gmail.com>
2025-09-10 16:39:32 -0500

    Actualización de M5L2

--- Assets/M5/M5L2/Scripts/Player.cs
+++ Assets/M5/M5L2/Scripts/Player.cs
@@ -6,15 +6,23 @@ namespace M5L2
     {
         public int points;
 
+        #region Pag 77
+        public Projectile projectilePrefab;
+        #endregion
 
-        void Start()
-        {
+        #region Pag 79
+        public float shootInterval = 0.5f;
+        public float shootTimer;
+        #endregion
 
-        }
+        #region Pag 86
+        public Transform shootPoint;
+        #endregion
 
         void Update()
         {
             Move();
+            Shoot();
         }
 
         void Move()
@@ -26,5 +34,40 @@ namespace M5L2
                 transform.position = realPos;
             }
         }
+
+        #region Pag 77
+        // void Shoot()
+        // {
+        //     Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        // }
+        #endregion
+
+        #region Pag 79
+        // void Shoot()
+        // {
+        //     shootTimer -= Time.deltaTime;
+
+        //     if (shootTimer <= 0)
+        //     {
+        //         Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        //         shootTimer = shootInterval;
+        //     }
+        // }
+        #endregion
+        
+        #region Pag 86
+        void Shoot()
+        {
+            shootTimer -= Time.deltaTime;
+
+            if (shootTimer <= 0)
+            {
+                Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+
+                shootTimer = shootInterval;
+            }
+        }
+        # endregion
     }
 }
---
+++ Assets/M5/M5L2/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pag. 83
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (Vector2)transform.position + speed * Time.deltaTime * Vector2.up;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Obstacle")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            Debug.Log("Destruyo el Obstacle");
+        }
+    }
+}
