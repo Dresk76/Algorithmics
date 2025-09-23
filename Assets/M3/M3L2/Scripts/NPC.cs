d2b77d05e252e139f2de56d0a0e841acca8b790e
Felipe Agudelo <felipeagudeloolaya@gmail.com>
2025-09-23 15:08:06 -0500

    Actualización de nivel M3L2

--- Assets/M3/M3L2/Scripts/NPC.cs
+++ Assets/M3/M3L2/Scripts/NPC.cs
@@ -8,6 +8,9 @@ public class NPC : MonoBehaviour
     // Nivel de los NPC
     int level = 5;
 
+    // Velocidad de los NPC
+    public float velocity = 1.2f;
+
 
     void Start()
     {
@@ -16,5 +19,17 @@ public class NPC : MonoBehaviour
 
         // Enviar el valor de salud a la consola para verificar que el cálculo es correcto.
         print("Salud del NPC actualizada: " + health);
+    }
+
+    void Update()
+    {
+        // Crear un Vector3 para almacenar la nueva posición
+        Vector3 newPosition = transform.position;
+
+        // Modificar la posición en el eje Z según la velocidad y el tiempo entre fotogramas
+        newPosition.z += velocity * Time.deltaTime;
+
+        // Actualizar la posición del NPC
+        transform.position = newPosition;
     }
 }
